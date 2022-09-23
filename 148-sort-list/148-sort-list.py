e3fbc4888a1e332f448429ebdd99362b2a4ae940
Hussain Omer <67332652+hussaino03@users.noreply.github.com>
2022-09-23 12:27:13 -0400

    Time: 1042 ms (47.76%), Space: 44.9 MB (13.74%) - LeetHub

---
+++ 148-sort-list/148-sort-list.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return 
+        else:
+            lst = []
+            curr = head
+            while curr is not None:
+                lst.append(curr.val)
+                curr = curr.next
+                
+            lst.sort()
+            newHead = ListNode()
+            pointer = newHead
+            for i in lst:
+                pointer.next = ListNode(i)
+                pointer = pointer.next
+            return newHead.next
+                    
