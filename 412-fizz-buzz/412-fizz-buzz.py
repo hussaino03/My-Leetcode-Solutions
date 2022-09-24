2abcc538035470acd3a7bb0a048be19fddce4c47
Hussain Omer <67332652+hussaino03@users.noreply.github.com>
2022-09-24 14:35:54 -0400

    Time: 112 ms (5.21%), Space: 15.2 MB (17.65%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -0,0 +1,25 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        lst = []
+        i = 1
+        while i <= n:
+            lst.append(str(i))
+            i += 1
+            
+        for i in range(len(lst)):
+            lst[i] = int(lst[i])
+            if lst[i] % 3 == 0 and lst[i] % 5 == 0:
+                lst[i] = "FizzBuzz"
+                lst[i] = str(lst[i])
+                
+            elif lst[i] % 3 == 0:
+                lst[i] = "Fizz"
+                lst[i] = str(lst[i])
+                
+            elif lst[i] % 5 == 0:
+                lst[i] = "Buzz"
+                lst[i] = str(lst[i])
+                
+        return [str(x) for x in lst]
+                
+            
