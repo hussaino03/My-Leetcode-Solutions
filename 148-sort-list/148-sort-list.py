522ad969c9e2a180ad21be9ee616fa8f85d61669
Hussain Omer <67332652+hussaino03@users.noreply.github.com>
2022-09-27 13:01:38 -0400

    Time: 2685 ms (5.01%), Space: 45.1 MB (8.50%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -12,12 +12,29 @@ class Solution:
         while current:
             linkedList.append(current.val)
             current = current.next
-            
-        linkedList.sort()
         
+        res = self.msort2(linkedList)
         pointer = newHead = ListNode()
-        for i in linkedList:
+        for i in res:
             pointer.next = ListNode(i)
             pointer = pointer.next
         return newHead.next
+    
+    def msort2(self, x):
+        if len(x) < 2:
+            return x
+        result = []          
+        mid = int(len(x) / 2)
+        y = self.msort2(x[:mid])
+        z = self.msort2(x[mid:])
+        while (len(y) > 0) and (len(z) > 0):
+            if y[0] > z[0]:
+                result.append(z[0])
+                z.pop(0)
+            else:
+                result.append(y[0])
+                y.pop(0)
+        result += y
+        result += z
+        return result
                     
